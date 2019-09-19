3d23cdadd72d14a17990525f614b4be6d72935d5
BarcinoLechuguino <angotov@gmail.com>
2019-09-19 04:06:28 +0200

    Vec3 module/class first version

--- Vectors/Vectors/Ein Klass.h
+++ Vectors/Vectors/Ein Klass.h
@@ -1,1 +1,141 @@
 #pragma once
+#include <iostream>
+
+//Useful commands: Ctrl + f + f (Searches the whole project for whatever you ask it to), Ctrl + r + r (Changes any text in the whole solution/item, like the name  of variables and the such).
+
+using namespace std;
+
+//Standarizes the variables and they can be changed whenever in the main() without having to change them all.
+template <class Vec> class Vec3
+{
+private:
+	Vec x, y, z;
+
+public:
+	//Constructors are functions that are run when the class is called.
+	Vec3(Vec posX, Vec posY, Vec posZ) //Sets the introduced values as the coordinates.
+	{
+		x = posX;
+		y = posY;
+		z = posZ;
+	}
+
+	Vec3() //In case no values are introduced the coordinates will be all set to 0.
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+
+	Vec3(Vec posS)
+	{
+
+	}
+
+	//Destructors are functions that are run when the class stops being called.
+	~Vec3() 
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+
+	//Get() function that returns a coordinate when called. It's const because it only returns a number, it does not modify it.
+	Vec getx() const { return x; }
+	Vec gety() const { return y; }
+	Vec getz() const { return z; }
+
+	//Operator functions that calculate the addition and substraction of vectors as well as equaling and comparing them.
+	Vec operator + (Vec3 &v)
+	{
+		x = x + v.x;
+		y = y + v.y;
+		z = z + v.z;
+		return x, y, z;
+	}
+
+	Vec operator - (Vec3 &v)
+	{
+		x = x - v.x;
+		y = y - v.y;
+		z = z - v.z;
+		return x, y, z;
+	}
+
+	Vec operator += (Vec3 &v)
+	{
+		x += v.x;
+		y += v.y;
+		z += v.z;
+		return x, y, z;
+	}
+
+	Vec operator -= (Vec3 &v)
+	{
+		x -= v.x;
+		y -= v.y;
+		z -= v.z;
+		return x, y, z;
+	}
+
+	Vec operator = (Vec3 &v)
+	{
+		x = v.x;
+		y = v.y;
+		z = v.z;
+		return x, y, z;
+	}
+
+	void operator == (Vec3 &v)
+	{
+		if (x == v.x && y == v.y && z == v.z) { cout << "Both vectors are equal." << endl; }
+		else { cout << "Both vectors are not equal" << endl; }
+		//if (x != v.x || y != v.y || z != v.z) { cout << "Both vectors are not equal" << endl; }
+	}
+
+	Vec normalize()
+	{
+		Vec Module = sqrt(x*x + y*y + z*z);
+
+		x = x / Module;
+		y = y / Module;
+		z = z / Module;
+
+		return x, y, z;
+	}
+
+	void setToZero() { x = 0; y = 0; z = 0; }
+
+	void getVector() { cout << "X is " << x << ", Y is " << y << " and Z is " << z << ". The vector will be V = ( " << x << ", " << y << ", " << z << " )" << endl; }
+
+	void isZero() //This function checks which coordinates of a given vector are 0.
+	{
+		bool allZero = false;
+
+		if (x == 0 && y == 0 && z == 0) { cout << "All the coordinates are equal to 0" << endl; allZero = true; }
+		if (x != 0 && y != 0 && z != 0) { cout << "None of the coordinates are equal to 0" << endl; allZero = false; }
+
+		if (allZero == false)
+		{
+			if (x == 0 && y != 0 && z != 0) { cout << "Only X is equal to 0" << endl; }
+			if (y == 0 && x != 0 && z != 0) { cout << "Only Y is equal to 0" << endl; }
+			if (z == 0 && x != 0 && y != 0) { cout << "Only Z is equal to 0" << endl; }
+			if (x == 0 && y == 0 && z != 0) { cout << "Only X and Y are equal to 0" << endl; }
+			if (x == 0 && y != 0 && z == 0) { cout << "Only X and Z are equal to 0" << endl; }
+			if (x != 0 && y == 0 && z == 0) { cout << "Only Y and Z are equal to 0" << endl; }
+		}
+
+		allZero = false;
+	}
+
+	Vec distanceTo(Vec3 &v) 
+	{
+		Vec powerOfX = (v.x - x) * (v.x - x);
+		Vec powerOfY = (v.y - y) * (v.y - y);
+		Vec powerOfZ = (v.z - z) * (v.z - z);
+		Vec distance = sqrt(powerOfX + powerOfY + powerOfZ);
+
+		cout << "The distance between the two given points is " << distance << endl;
+		return distance;
+	}
+};
--- Vectors/Vectors/Vec3 Module.cpp
+++ Vectors/Vectors/Vec3 Module.cpp
@@ -1,67 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-
-using namespace std;
-
-template <class Vec> class Vec3 //Standarizes the variables
-{
-private:
-	Vec x, y, z;
-
-public:
-	Vec3(Vec posx, Vec posy, Vec posz) //Constructors are processes that are processed when the class is called.
-	{
-		x = posx; 
-		y = posy;
-		z = posz;
-	}
-
-	Vec3()
-	{
-		x = 0;
-		y = 0;
-		z = 0;
-	}
-
-	Vec3()
-	{
-		
-	}
-
-	Vec getx() const { return x; }
-	Vec gety() const { return y; }
-	Vec getz() const { return z; }
-
-	Vec operator + (Vec3 &v) { x = x + v.x; y = y + v.y; z = z + v.z; return x, y, z; }
-	Vec operator - (Vec3 &v) { x = x - v.x; y = y - v.y; z = z - v.z; return x, y, z; }
-	Vec operator += (Vec3 &v) { x += v.x; y += v.y; z += v.z; return x, y, z; }
-	Vec operator -= (Vec3 &v) { x -= v.x; y -= v.y; z -= v.z; return x, y, z; }
-	Vec operator = (Vec3 &v) { x = v.x; y = v.y; z = v.z; return x, y, z; }
-	Vec operator == (Vec &v) {}
-
-	Vec normalize() {}
-	Vec setToZero() { x = 0; y = 0; z = 0; }
-	Vec getVector() { cout << "X is " << x << " Y is " << y << " Z is " << z << "So the vector will be V = ( "<< x y z << " )"}
-	Vec isZero() 
-	{
-		if (x == 0 && y == 0 && z == 0) { cout << "x, y and z are all equal to 0" << endl; }
-		if (x == 0) { cout << "X is equal to 0" << endl; }
-		if (y == 0) { cout << "Y is equal to 0" << endl; }
-		if (z == 0) { cout << "Z is equal to 0" << endl; }
-		if (x == 0 && y == 0) { cout << "X and Y are equal to 0" << endl; }
-		if (x == 0 && z == 0) { cout << "X and Z are equal to 0" << endl; }
-		if (y == 0 && z == 0) { cout << "Y and Z are equal to 0" << endl; }
-		else (x != 0 && y != 0 && z != 0) { cout << "None of the coordinates are equal to 0" << endl; }
-	}
-	Vec distanceTo() {}
-};
+#include "Ein Klass.h"
 
 int main()
 {
-	Vec3 <int> Vector();
+	Vec3 <float> Vector(1, 2, 3);
+	Vec3 <float> VectorII(1, 2, 3);
+	Vec3 <float> VectorIII(3, 2, 1);
+	Vec3 <float> VectorIV(2, 4, 6);
+	Vec3 <float> VectorV(0, 0, 0);
+	Vec3 <float> VectorVI(1, 0, 0);
+	Vec3 <float> VectorVII(0, 1, 0);
+	Vec3 <float> VectorVIII(0, 0, 1);
+	Vec3 <float> VectorIX(1, 1, 0);
+	Vec3 <float> VectorX(1, 0, 1);
+	Vec3 <float> VectorXI(0, 1, 1);
+	Vec3 <float> emptyVector();
 
+	/*//Get Vector function examples
+	Vector.getVector();
+	VectorII.getVector();
+	VectorIII.getVector();
+	VectorIV.getVector();
+
+	//Get Functions
 	Vector.getx();
+	Vector.gety();
+	Vector.getz();
+
+	//Operator use example
+	Vector.operator+(VectorII);
+	Vector.getVector();
+
+	Vector.operator-(VectorII);
+	Vector.getVector();
+
+	Vector.operator+=(VectorII);
+	Vector.getVector();
+
+	Vector.operator-=(VectorII);
+	Vector.getVector();
+
+	Vector.operator=(VectorIII);
+	Vector.getVector();
+
+	Vector.operator==(VectorII);
+	Vector.operator==(VectorIII);
+
+	//isZero function example
+	Vector.isZero();
+	VectorIV.isZero();
+	VectorV.isZero();
+	VectorVI.isZero();
+	VectorVII.isZero();
+	VectorVIII.isZero();
+	VectorIX.isZero();
+	VectorX.isZero();
+	VectorXI.isZero();
+
+	//Set to Zero function example
+	Vector.setToZero();
+	Vector.getVector();
+
+	//Normalize function example
+	Vector.normalize();
+	Vector.getVector();
+
+	//DistanceTo function example
+	Vector.distanceTo(VectorIV);*/
 
 	system("pause");
 	return 0;
