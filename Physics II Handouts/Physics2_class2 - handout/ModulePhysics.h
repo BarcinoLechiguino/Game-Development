702313c52d67d1a6120254a2a74c7fa23ca0ff4b
BarcinoLechuguino <angotov@gmail.com>
2019-10-31 02:49:21 +0100

    Improved the comments

--- Physics II Handouts/Physics2_class2 - handout/ModulePhysics.h
+++ Physics II Handouts/Physics2_class2 - handout/ModulePhysics.h
@@ -22,8 +22,8 @@ class b2Body;
 class Object
 {	
 public:
-	b2Body* objectBody; //Pointer to b2Body
-	b2Vec2* GetPosition(); //Has to return a vector
+	b2Body* objectBody;				//Pointer to b2Body
+	b2Vec2* GetPosition();			//Has to return a vector
 	b2Vec2* GetCenterPosition();
 	float GetRotation();
 };
@@ -49,19 +49,19 @@ public:
 	{	
 		Object* Circle = new Object;
 		
-		b2BodyDef bodyCircle; //Declares the frame for the geometrical form.
-		bodyCircle.type = b2_dynamicBody; //Defines the type of the frame/body as a dynamic body.
-		float radi = PIXEL_TO_METERS(radius); //Defines the radius variable of the circle. PIXEL_TO_METERS converts pixels to meters. In this environment we have set 50 pixels per meter.
-		bodyCircle.position.Set(PIXEL_TO_METERS(posX), PIXEL_TO_METERS(posY)); //Sets the position where the shape will be created.
+		b2BodyDef bodyCircle;													//Declares the frame for the geometrical form.
+		bodyCircle.type = b2_dynamicBody;										//Defines the type of the frame/body as a dynamic body.
+		float radi = PIXEL_TO_METERS(radius);									//Defines the radius variable of the circle. PIXEL_TO_METERS converts pixels to meters. In this environment we have set 50 pixels per meter.
+		bodyCircle.position.Set(PIXEL_TO_METERS(posX), PIXEL_TO_METERS(posY));	//Sets the position where the shape will be created.
 
-		Circle->objectBody = world->CreateBody(&bodyCircle); //Creates a body given a definition.
+		Circle->objectBody = world->CreateBody(&bodyCircle);					//Creates a body given a definition.
 
-		b2CircleShape shapeCircle; //We define that we want to create a circle and name it shape
-		shapeCircle.m_radius = radi; //We set the radius with the radi variable we have declared before and translated to meters.
-		b2FixtureDef fixture; //Definition of the fixture. This has multiple elements (shape(circle), density(weight) and friction(how it drags along other surfaces))
-		fixture.shape = &shapeCircle; //Defines which shape will the fixture have.
+		b2CircleShape shapeCircle;												//We define that we want to create a circle and name it shape
+		shapeCircle.m_radius = radi;											//We set the radius with the radi variable we have declared and translated to meters.
+		b2FixtureDef fixture;													//Definition of the fixture. This has multiple elements (shape(circle), density(weight) and friction(how it drags along other surfaces))
+		fixture.shape = &shapeCircle;											//Defines which shape will the fixture have.
 
-		Circle->objectBody->CreateFixture(&shapeCircle, 1.0f); //Fixture definition automatically updates the mass of the body. As many fixtures as wanted can be added.
+		Circle->objectBody->CreateFixture(&shapeCircle, 1.0f);					//Fixture definition automatically updates the mass of the body. As many fixtures as wanted can be added.
 		
 		return Circle;
 	}
@@ -96,33 +96,33 @@ public:
 		// remember to convert them from pixels to meters!
 		chainVectors[numVectors];
 
-		int ChainPoints[512]; //Revise if size can be passed as an argument.
+		int ChainPoints[512];							//Revise if size can be passed as an argument.
 		
-		for (int i = 0; i < numPoints; i++) //Loop that iterates to fill an array with the passed argument's array of values. Had to be done this way due to the fact that an array index cannot be passed as an argument individually and then matched in the function.
+		for (int i = 0; i < numPoints; i++)				//Loop that iterates to fill an array with the passed argument's array of values. Had to be done this way due to the fact that an array index cannot be passed as an argument individually and then matched in the function.
 		{
 			ChainPoints[i] = chainPoints[i];
 		}
 
-		for (int i = 0; i < numVectors; i++) //Iterates the array of vectors to assign each one of them a position on screen.
+		for (int i = 0; i < numVectors; i++)			//Iterates the array of vectors to assign each one of them a position on screen.
 		{
 			chainVectors[i].Set(PIXEL_TO_METERS(chainPoints[i * 2]), PIXEL_TO_METERS(chainPoints[i * 2 + 1])); 
 		}
 
-		Object* Chain = new Object;
+		Object*	Chain = new Object;
 
-		b2BodyDef chainBody;													// Holds ALL the data required to construct a rigid body. In this case a chain.
-		chainBody.type = b2_dynamicBody;										//Sets the chain body type to dynamic (interacts with the different forcess in the environment.
+		b2BodyDef	chainBody;													//Holds ALL the data required to construct a rigid body. In this case a chain.
+		chainBody.type = b2_dynamicBody;										//Sets the chain body type to dynamic (interacts with the different forcess in the environment).
 		chainBody.position.Set(PIXEL_TO_METERS(posX), PIXEL_TO_METERS(posY));	//Sets the position of the whole body when it is created on screen.
 
-		Chain->objectBody = world->CreateBody(&chainBody);	//Creates a rigid body given a body definition (the definition the chain body in this case.)
+		Chain->objectBody = world->CreateBody(&chainBody);		//Creates a rigid body given a body definition (the definition the chain body in this case.)
 
-		b2ChainShape chainShape;				//Defines a Chain shape and name it as shown.
-		b2FixtureDef chainFixture;				//Definition of the fixture. This has multiple elements (shape(circle), density(weight) and friction(how it drags along other surfaces)) 
-		//chainFixture.shape = &chainShape;		// Sets the fixture shape to the aforementioned chainShape. Called later in the Create fixture function
+		b2ChainShape chainShape;								//Defines a Chain shape and name it as shown.
+		b2FixtureDef chainFixture;								//Definition of the fixture. This has multiple elements (shape(circle), density(weight) and friction(how it drags along other surfaces)) 
+		//chainFixture.shape = &chainShape;						//Sets the fixture shape to the aforementioned chainShape. Called later in the Create fixture function
 
-		chainShape.CreateLoop(chainVectors, numVectors); //Adjusts the connectivity of the vertexs
+		chainShape.CreateLoop(chainVectors, numVectors);		//Adjusts the connectivity of the vertexs
 
-		Chain->objectBody->CreateFixture(&chainShape, 1.0f); //Fixture definition automatically updates the mass of the body. As many fixtures as wanted can be added.
+		Chain->objectBody->CreateFixture(&chainShape, 1.0f);	//Fixture definition automatically updates the mass of the body. As many fixtures as wanted can be added.
 
 		return Chain;
 	}
