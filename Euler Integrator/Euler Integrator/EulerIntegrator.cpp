a03058324b150a33aea25f01a2903a9f2e538b21
Ángel González <Angotov@gmail.com>
2019-11-15 12:36:20 +0100

    Euler integrator part 2

--- Euler Integrator/Euler Integrator/EulerIntegrator.cpp
+++ Euler Integrator/Euler Integrator/EulerIntegrator.cpp
@@ -13,9 +13,15 @@ public:
 	float z;
 };
 
-int dt = 1 / 30;
+class particle
+{
+public:
+	vec3d position;
+	vec3d speed;
+	vec3d acceleration;
+};
 
-void EulerIntegrator(vec3d iposition, vec3d ivelocity, vec3d fposition, float acceleration, int dt)
+void EulerIntegrator(vec3d& iposition, vec3d& ivelocity, vec3d& fposition, vec3d& fvelocity, float acceleration, float dt) //The value we want to "return" changed must be passed as reference so the variable passed as argument is changed.
 {
 	//y = yo + vo * dt
 	//v = vo + a * dt
@@ -23,19 +29,25 @@ void EulerIntegrator(vec3d iposition, vec3d ivelocity, vec3d fposition, float ac
 	fposition.y = iposition.y + ivelocity.y * dt;
 	fposition.z = iposition.z + ivelocity.z * dt;
 
+	iposition.x = fposition.x;
+	iposition.y = fposition.y;
+	iposition.z = fposition.z;
 
-	vec3d vel;
-	/*vel.x = velocity.x + acceleration * dt;
-	vel.y = velocity.y + acceleration * dt;
-	vel.z = velocity.z + acceleration * dt;*/
+	fvelocity.x = ivelocity.x + acceleration * dt;
+	fvelocity.y = ivelocity.y + acceleration * dt;
+	fvelocity.z = ivelocity.z + acceleration * dt;
+
+	ivelocity.x = fvelocity.x;
+	ivelocity.x = fvelocity.x;
+	ivelocity.x = fvelocity.x;
 }
 
 int main()
 {
 	vec3d ipos;
-	ipos.x = 0;
-	ipos.y = 0;
-	ipos.z = 0;
+	ipos.x = 0.0f;
+	ipos.y = 0.0f;
+	ipos.z = 0.0f;
 
 	vec3d ivel;
 	ivel.x = 5; 
@@ -44,11 +56,26 @@ int main()
 
 	vec3d fpos, fvel;
 
-	EulerIntegrator(ipos, ivel, fpos, 0.0f, dt);
+	float dt = 1.0f / 30.0f;
+
+
+	for (int i = 0; i < 4; i++)
+	{
+		EulerIntegrator(ipos, ivel, fpos, fvel, 1.0f, dt);
+
+		cout << "fpos is: (" << fpos.x << " " << fpos.y << " " << fpos.z << ")" << endl;
+		cout << "fvel is: (" << fvel.x << " " << fvel.y << " " << fvel.z << ")" << endl;
+	}
 
 	cout << "Initial position: (" << ipos.x << " " << ipos.y << " " << ipos.z << ")" << endl;
 
+	/*while (1)
+	{
+		EulerIntegrator(ipos, ivel, fpos, fvel, 1.0f, dt);
 
+		cout << "fpos is: (" << fpos.x << " " << fpos.y << " " << fpos.z << ")" << endl;
+		cout << "fvel is: (" << fvel.x << " " << fvel.y << " " << fvel.z << ")" << endl;
+	}*/
 
 	system("pause");
 	return 0;
