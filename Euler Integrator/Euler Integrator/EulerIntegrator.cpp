c5fec23aeb95c01e448d7a944e87dff965e2ca4f
BarcinoLechuguino <angotov@gmail.com>
2019-12-03 19:16:40 +0100

    The final velocity and the final acceleration now show on the console.

--- Euler Integrator/Euler Integrator/EulerIntegrator.cpp
+++ Euler Integrator/Euler Integrator/EulerIntegrator.cpp
@@ -65,6 +65,8 @@ int main()
 
 	cout << "Initial position: (" << ipos.x << " " << ipos.y << " " << ipos.z << ")" << endl;
 
+	cout << endl;
+
 	for (int i = 0; i < 4; i++)
 	{
 		EulerIntegrator(ipos, ivel, fpos, fvel, acceleration, 1);								//EulerIntegrator() function declaration.
@@ -73,7 +75,13 @@ int main()
 		cout << "	fvel is: (" << fvel.x << " " << fvel.y << " " << fvel.z << ")" << endl;
 	}
 
+	cout << endl;
+
 	cout << "Final position: (" << ipos.x << " " << ipos.y << " " << ipos.z << ")" << endl;
+	cout << "Final velocity: (" << ivel.x << " " << ivel.y << " " << ivel.z << ")" << endl;
+	cout << "Final acceleration: (" << acceleration.x << " " << acceleration.y << " " << acceleration.z << ")" << endl;
+
+	cout << endl;
 
 	system("pause");
 	return 0;
